ef0e483dce2cb01d3a98a1de43c4757e83168b60
ayushi0294 <ayushigupta0294@gmail.com>
2018-07-25 14:13:19 +0530

    gate role premissions https://laravel-news.com/authorization-gates

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Providers;
+use App\Post;
+use Illuminate\Support\ServiceProvider;
+use Gate;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPostPolicies();
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+
+    public function registerPostPolicies()
+    {
+        // dd('registerPostPolicies');
+        Gate::define('create-post', function ($user) {
+            return $user->hasAccess(['create-post']);
+        });
+        Gate::define('update-post', function ($user, Post $post) {
+            return $user->hasAccess(['update-post']) or $user->id == $post->user_id;
+        });
+        Gate::define('publish-post', function ($user) {
+            return $user->hasAccess(['publish-post']);
+        });
+        Gate::define('see-all-drafts', function ($user) {
+            return $user->inRole('editor');
+        });
+    }
+
+
+}
